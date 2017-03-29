d9dee4a093687b5eabe3749dd863e377e43ee7bd
Luzimar Oliveira <Luzimar Oliveira>
2017-03-29 20:57:14 -0300

    commit inicial

---
+++ FormsToMvvm/FormsToMvvm/ViewModels/MainViewModel.cs
@@ -0,0 +1,111 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace FormsToMvvm.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _keyWord;
+        private List<string> _sugestoes = new List<string>();
+        private bool _isVisible;
+        private List<string> _meusCortes = new List<string>();
+        private string _corteSelecionado;
+        public string CorteSelecionado
+        {
+            get
+            { return _corteSelecionado; }
+            set
+            {
+                _corteSelecionado = value;
+                OnPropertyChanged();
+            }
+        }
+        public string KeyWord
+        {
+            get { return _keyWord; }
+            set
+            {
+                _keyWord = value;
+                OnPropertyChanged();
+            }
+        }
+        public List<string> Sugestoes
+        {
+            get { return _sugestoes; }
+            set
+            {
+                _sugestoes = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsVisible
+        {
+            get { return _isVisible; }
+            set
+            {
+                _isVisible = value;
+                OnPropertyChanged();
+            }
+        }
+        public Command FiltrarCommand
+        {
+            get
+            {
+                return new Command(Filtrar);
+            }
+        }
+        public Command AdicionarCommand
+        {
+            get
+            {
+                return new Command(Adicionar);
+            }
+        }
+        public List<string> MeusCortes
+        {
+            get
+            {
+                return _meusCortes;
+            }
+            set
+            {
+                _meusCortes = value;
+                OnPropertyChanged();
+            }
+        }
+        private List<string> cortes = new List<string>
+        {
+            "Social","Só máquina","Militar","Degradê","Surfista"
+        };
+        private void Filtrar()
+        {
+            if (_keyWord.Length >= 1)
+            {
+                Sugestoes = cortes.Where(c => c.ToLower().Contains(_keyWord.ToLower())).ToList();
+                IsVisible = true;
+            }
+            else
+            {
+                IsVisible = false;
+            }
+
+        }
+        private void Adicionar()
+        {
+            var c = _meusCortes;
+            c.Add(_corteSelecionado);
+            MeusCortes = c.ToList();
+            IsVisible = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FormsToMvvm/FormsToMvvm/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using FormsToMvvm.ViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace FormsToMvvm
+{
+    public partial class MainPage : ContentPage
+    {
+        //private List<string> cortes = new List<string>
+        //{
+        //    "Social","Só máquina","Militar","Degradê","Surfista"
+        //};
+        //private ObservableCollection<string> meus_cortes = new ObservableCollection<string>();
+        public MainPage()
+        {
+            InitializeComponent();
+            //    BindingContext = new MainViewModel();
+        }
+
+        private void SelecionarCorte(object sender, ItemTappedEventArgs e)
+        {
+            var vm = BindingContext as MainViewModel;
+            vm.AdicionarCommand.Execute(null);
+            //var corteSelecionado = e.Item as string;
+            //meus_cortes.Add(corteSelecionado);
+            //lstCortes.ItemsSource = meus_cortes;
+            //lst_sugestao_cortes.IsVisible = false;
+        }
+
+    }
+}
